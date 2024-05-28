d2fb4e4ce4ef2c7e43e78b91dd264d091e0251d2
Neil Nelanuthala <neilnelan95@gmail.com>
2024-05-28 14:31:51 -0400

    chore: Add Gradle project files and configurations

---
+++ app/src/main/java/simple/gradle/java/project/Controller.java
@@ -0,0 +1,14 @@
+package simple.gradle.java.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Spring Boot App!";
+    }
+
+}
